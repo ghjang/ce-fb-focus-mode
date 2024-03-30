321944e5805fba49821da7b1a13ff7598c9d22b1
ghjang <jmxexpert@hotmail.com>
2024-03-30 22:14:06 +0900

    컨텐트 스크립트 코드를 클래스로 리팩터링

--- scripts/content.js
+++ scripts/content.js
@@ -1,75 +1,83 @@
-const textsToHide = ['광고', '릴스 및 짧은 동영상'];
+class FacebookFocusMode {
+    isActivated;
+    textsToHide;
+    #throttledHideDivsWithSpecificSpans;
 
-const hideDivsWithSpecificSpans = () => {
-    console.log('hideDivsWithSpecificSpans called');
+    constructor(textsToHide = []) {
+        this.isActivated = false;
+        this.textsToHide = textsToHide;
+        this.#throttledHideDivsWithSpecificSpans = _.throttle(this.#hideDivsWithSpecificSpans, 100);
+    }
 
-    const divs = document.querySelectorAll('div[data-pagelet^="FeedUnit_"]:not([style*="display: none"])');
-    divs.forEach(div => {
-        const spans = div.getElementsByTagName('span');
-        Array.from(spans).forEach(span => {
-            if (textsToHide.includes(span.textContent.trim())) {
-                div.style.display = 'none';
-                div.setAttribute('data-fb-focus-mode', '');
+    #hideDivsWithSpecificSpans = () => {
+        console.log('hideDivsWithSpecificSpans called');
+
+        const divs = document.querySelectorAll('div[data-pagelet^="FeedUnit_"]:not([style*="display: none"])');
+        divs.forEach(div => {
+            const spans = div.getElementsByTagName('span');
+            Array.from(spans).forEach(span => {
+                if (this.textsToHide.includes(span.textContent.trim())) {
+                    div.style.display = 'none';
+                    div.setAttribute('data-fb-focus-mode', '');
+                }
+            });
+        });
+    };
+
+    #hideStoriesPageLet = () => {
+        const storiesPageLet = document.querySelector('div[data-pagelet="Stories"]');
+        if (storiesPageLet) {
+            storiesPageLet.style.display = this.isActivated ? 'none' : '';
+        }
+    };
+
+    #hideSidebars = () => {
+        const sidebars = document.querySelectorAll('div[data-pagelet^="LeftRail"], div[data-pagelet^="RightRail"]');
+        sidebars.forEach(sidebar => {
+            const sideDiv = sidebar.closest('div[role="navigation"], div[role="complementary"]');
+            if (sideDiv) {
+                sideDiv.style.display = this.isActivated ? 'none' : '';
             }
         });
-    });
-};
+    };
 
+    #updatePageState = () => {
+        window.removeEventListener('scroll', this.#throttledHideDivsWithSpecificSpans);
 
-const hideStoriesPageLet = (hide = true) => {
-    const storiesPageLet = document.querySelector('div[data-pagelet="Stories"]');
-    if (storiesPageLet) {
-        storiesPageLet.style.display = hide ? 'none' : '';
-    }
-};
-
-
-const hideSidebars = (hide = true) => {
-    const sidebars = document.querySelectorAll('div[data-pagelet^="LeftRail"], div[data-pagelet^="RightRail"]');
-    sidebars.forEach(sidebar => {
-        const sideDiv = sidebar.closest('div[role="navigation"], div[role="complementary"]');
-        if (sideDiv) {
-            sideDiv.style.display = hide ? 'none' : '';
+        if (this.isActivated) {
+            this.#throttledHideDivsWithSpecificSpans();
+            window.addEventListener('scroll', this.#throttledHideDivsWithSpecificSpans);
         }
-    });
-};
 
+        this.#hideStoriesPageLet();
+        this.#hideSidebars();
+    };
 
-let g_isActivated = false;
-
-const throttledHideDivsWithSpecificSpans = _.throttle(hideDivsWithSpecificSpans, 100);
-
-const updatePageState = () => {
-    window.removeEventListener('scroll', throttledHideDivsWithSpecificSpans);
-
-    if (g_isActivated) {
-        throttledHideDivsWithSpecificSpans();
-        window.addEventListener('scroll', throttledHideDivsWithSpecificSpans);
-    }
-
-    hideStoriesPageLet(g_isActivated);
-    hideSidebars(g_isActivated);
-};
-
-const handleKeyDown = (event) => {
-    if (event.ctrlKey && event.shiftKey && event.key === 'F') {
-        g_isActivated = !g_isActivated;
-        updatePageState();
-    }
-};
-
-
-window.addEventListener('keydown', handleKeyDown);
-
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'urlChanged') {
-        window.removeEventListener('keydown', handleKeyDown);
-        window.removeEventListener('scroll', throttledHideDivsWithSpecificSpans);
-
-        if (request.url === 'https://www.facebook.com/') {
-            window.addEventListener('keydown', handleKeyDown);
-            updatePageState();
+    #handleKeyDown = (event) => {
+        if (event.ctrlKey && event.shiftKey && event.key === 'F') {
+            this.isActivated = !this.isActivated;
+            this.#updatePageState();
         }
+    };
+
+    #handleMessage = (request, sender, sendResponse) => {
+        if (request.message === 'urlChanged') {
+            window.removeEventListener('keydown', this.#handleKeyDown);
+            window.removeEventListener('scroll', this.#throttledHideDivsWithSpecificSpans);
+
+            if (request.url === 'https://www.facebook.com/') {
+                window.addEventListener('keydown', this.#handleKeyDown);
+                this.#updatePageState();
+            }
+        }
+    };
+
+    run() {
+        window.addEventListener('keydown', this.#handleKeyDown);
+        chrome.runtime.onMessage.addListener(this.#handleMessage);
     }
-});
+}
+
+
+const facebookFocusMode = new FacebookFocusMode(['광고', '릴스 및 짧은 동영상']);
+facebookFocusMode.run();
